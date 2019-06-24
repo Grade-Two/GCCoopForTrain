ccd822fd70cc85a6d156f7abb9ce0f56bd05fa66
GradeTwo <282345708@qq.com>
2019-06-24 13:51:42 +0800

    Create install_tf.sh
    Bash to install anaconda3 and tensorflow-gpu.

---
+++ install_tf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install anaconda3
+cd ..
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+chmod a+x Anaconda3-2019.03-Linux-x86_64.sh
+(echo 
+echo "q"
+echo "yes"
+echo 
+echo "yes") | ./Anaconda3-2019.03-Linux-x86_64.sh
+source ~/.bashrc
+
+# install tensorflow
+echo "y" | conda install tensorflow
