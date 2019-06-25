585aa881a2590cb8eb9533f6b0187f284eebbacc
GradeTwo <282345708@qq.com>
2019-06-25 10:45:55 +0800

    Update install_tf.sh

--- install_tf.sh
+++ install_tf.sh
@@ -4,11 +4,13 @@
 cd ..
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 chmod a+x Anaconda3-2019.03-Linux-x86_64.sh
-(echo 
-echo "q"
+(
+echo 
+# echo "q"
 echo "yes"
 echo 
-echo "yes") | ./Anaconda3-2019.03-Linux-x86_64.sh
+echo "yes"
+) | ./Anaconda3-2019.03-Linux-x86_64.sh
 source ~/.bashrc
 
 # install tensorflow
