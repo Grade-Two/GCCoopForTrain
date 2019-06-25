afb7677f61b29ce1b5068228c4dbe2c26fd22a40
GradeTwo <282345708@qq.com>
2019-06-25 13:14:58 +0800

    Update single_device.sh

--- single_device.sh
+++ single_device.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # download cifar10 if necessery
-cd ResNet
 if [ ! -d "cifar-10-batches-py/" ];then
 wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -zxvf cifar-10-python.tar.gz
