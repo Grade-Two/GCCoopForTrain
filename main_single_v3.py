e3f774c5d9a3c9745b17d0780d35703d347fa4e7
GradeTwo <282345708@qq.com>
2019-07-02 13:54:41 +0800

    Update main_single_v3.py

--- main_single_v3.py
+++ main_single_v3.py
@@ -53,19 +53,24 @@ def cnn_model_fn(features,labels,mode):
     return tf.contrib.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def main():
+def main(unused_args):
     mnist = tf.contrib.learn.datasets.load_dataset("mnist")
     train_data = mnist.train.images
     train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
     eval_data = mnist.test.images
     eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-    mnist_classifier = tf.contrib.estimator.Estimator(model_fn=cnn_model_fn, model_dir="mnist_convnet_model")
+    sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    config = tf.contrib.estimator.RunConfig(session_config=sess_config, train_distribute=None,
+                                            model_dir="mnist_convnet_model")
+    mnist_classifier = tf.contrib.estimator.Estimator(model_fn=cnn_model_fn, model_dir="mnist_convnet_model",
+                                                      config=config)
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=10)
     train_input_fn = tf.contrib.estimator.inputs.numpy_input_fn(x={"x": train_data}, y=train_labels, batch_size=20,
                                                                 num_epochs=None, shuffle=True)
-    mnist_classifier.train(input_fn=train_input_fn, steps=2000)
-    eval_input_fn = tf.contrib.estimator.inputs.numpy_input_fn(x={"x": eval_data}, y=eval_labels, num_epochs=1, shuffle=False)
+    mnist_classifier.train(input_fn=train_input_fn, steps=2000, hook=[logging_hook])
+    eval_input_fn = tf.contrib.estimator.inputs.numpy_input_fn(x={"x": eval_data}, y=eval_labels, num_epochs=1,
+                                                               shuffle=False)
     eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
     print(eval_results)
 
